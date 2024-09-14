257bdeea7224836f28aa4f04a6a8485315f00242
Silas Owu <owusilasyp00@gmail.com>
2024-09-14 13:19:06 -0700

    📈 Feat: integrate api facade service to display post list

--- src/app/views/posts-page/posts-page.component.ts
+++ src/app/views/posts-page/posts-page.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IPost } from '../../model/post.interface';
 import { ApiFacadeService } from '../../model/services/api-facade.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-posts-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './posts-page.component.html',
   styleUrl: './posts-page.component.scss'
 })
@@ -20,7 +21,6 @@ export class PostsPageComponent {
   ngOnInit():void{
     this.apiFacade.fetchPosts()
 
-    // this.posts$ = this.apiFacade.posts$;
-    this.posts$?.subscribe(posts => console.log(posts));
+    this.posts$ = this.apiFacade.posts$;
   }
 }
