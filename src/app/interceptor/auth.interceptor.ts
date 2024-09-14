4e33a5c38d9fe0da318759abd2a7a2462252e105
Silas Owu <owusilasyp00@gmail.com>
2024-09-14 07:27:20 -0700

    🤡 Feat: create an auth interceptor to handle authentication and debugging

--- src/app/interceptor/auth.interceptor.ts
+++ src/app/interceptor/auth.interceptor.ts
@@ -1,5 +1,26 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+   HttpRequest, HttpHandler
+  ,HttpEvent,HttpInterceptor }
+  from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', 'Bearer mock-jwt-token')
+    });
+
+    console.log('Outgoing HTTP request', authReq);
+
+    return next.handle(authReq).pipe(
+      tap({
+        next: event => console.log('HTTP response', event),
+        error: error => console.error('HTTP error', error)
+      })
+    );
+  }
+
+}
