1ae687be01789fa75e47b06d414de099ae5d6b94
Silas Owu <owusilasyp00@gmail.com>
2024-09-14 09:39:46 -0700

    🚚 Chore:create routes to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PostsPageComponent } from './views/posts-page/posts-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'post-detail-page/:id',
+    loadComponent: () =>
+    import('./views/post-detail-page/post-detail-page.component')
+    .then(m => m.PostDetailPageComponent)
+  },
+  {path: '**', component: PostsPageComponent}
+];
