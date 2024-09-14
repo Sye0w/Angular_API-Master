e833c5d74dafac918acc19754f056c4ec4a57f67
Silas Owu <owusilasyp00@gmail.com>
2024-09-14 09:18:13 -0700

    🗃️ Chore: update caching mechanisms

--- src/app/model/services/caching.service.ts
+++ src/app/model/services/caching.service.ts
@@ -4,28 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CachingService {
-  private cache: { [key: string]: { data: any, timestamp: number } } = {};
-  private cachedDuration = 5 * 60 * 1000;
+  private cache: { [key: string]: { data: any; timestamp: number } } = {};
+  private cacheDuration = 5 * 60 * 1000;
 
-  constructor() { }
-
-  set(key: string, data: any):void {
+  set(key: string, data: any): void {
     this.cache[key] = {
       data,
       timestamp: Date.now()
-    }
+    };
   }
 
-  get(key: string):any | null {
+  get(key: string): any | null {
     const cached = this.cache[key];
     if (!cached) {
+      return null;
+    }
+
+    const isExpired = Date.now() - cached.timestamp > this.cacheDuration;
+    if (isExpired) {
+      delete this.cache[key];
       return null;
     }
 
     return cached.data;
   }
 
-  clear(): void{
+  clear(): void {
     this.cache = {};
   }
 }
