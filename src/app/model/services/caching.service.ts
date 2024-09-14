044aa0a26acb5ff2d9c5e680f9468df457d79eba
Silas Owu <owusilasyp00@gmail.com>
2024-09-14 08:18:39 -0700

    🗃️ Chore: add caching mechanisms to optimize to optimize api usage and performance

--- src/app/model/services/caching.service.ts
+++ src/app/model/services/caching.service.ts
@@ -4,6 +4,28 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CachingService {
+  private cache: { [key: string]: { data: any, timestamp: number } } = {};
+  private cachedDuration = 5 * 60 * 1000;
 
   constructor() { }
+
+  set(key: string, data: any):void {
+    this.cache[key] = {
+      data,
+      timestamp: Date.now()
+    }
+  }
+
+  get(key: string):any | null {
+    const cached = this.cache[key];
+    if (!cached) {
+      return null;
+    }
+
+    return cached.data;
+  }
+
+  clear(): void{
+    this.cache = {};
+  }
 }
