d9bf623ac6104349750588731ea73103a1ebd0de
Silas Owu <owusilasyp00@gmail.com>
2024-09-14 08:44:53 -0700

    ✏️ Chore: update api-client service

--- src/app/model/services/api-client.service.ts
+++ src/app/model/services/api-client.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/env';
-import { catchError, Observable, retry, throwError } from 'rxjs';
+import { catchError, Observable, retry } from 'rxjs';
 import { IPost } from '../post.interface';
 import { IComment } from '../comment.interface';
 import { ErrorHandlingService } from './error-handling.service';
+// import { CachingService } from './caching.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,23 +14,52 @@ export class ApiClientService {
   private apiUrl = environment.apiUrl
 
   constructor(private http: HttpClient,
+    // private cachingService: CachingService,
     private errorHandler: ErrorHandlingService
   ) { }
 
-  fetchPost(): Observable<IPost[]>{
+  fetchPosts(): Observable<IPost[]>{
     return this.http.get<IPost[]>(`${this.apiUrl}/posts`).pipe(
       retry(3),
       catchError((error: HttpErrorResponse) => this.errorHandler.handleError(error))
     );
   }
 
-  fetchComments():Observable<IComment[]>{
-    return this.http.get<IComment[]>(`${this.apiUrl}/comments`)
+  fetchPostById(postId: number): Observable<any>{
+    return this.http.get(`${this.apiUrl}/posts/${postId}`)
+    .pipe(
+      retry(3),
+      catchError((error: HttpErrorResponse) => this.errorHandler.handleError(error))
+    );
+  }
+
+  fetchComments( postId: number):Observable<IComment[]>{
+    return this.http.get<IComment[]>(`${this.apiUrl}/posts/${postId}/comments`)
     .pipe(
       retry(3),
       catchError((error: HttpErrorResponse) => this.errorHandler.handleError(error))
     )
   }
 
+  createPost(post: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/posts`, post)
+      .pipe(
+        catchError((error: HttpErrorResponse) => this.errorHandler.handleError(error))
+      );
+  }
+
+  updatePost(id: number, post: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/posts/${id}`, post)
+      .pipe(
+        catchError((error: HttpErrorResponse) => this.errorHandler.handleError(error))
+      );
+  }
+
+  deletePost(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/posts/${id}`)
+      .pipe(
+        catchError((error: HttpErrorResponse) => this.errorHandler.handleError(error))
+      );
+  }
 
 }
