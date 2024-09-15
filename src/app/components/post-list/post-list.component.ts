a7e01dfc69b180a70ca07e415175b7257b269ef4
Silas Owu <owusilasyp00@gmail.com>
2024-09-15 05:40:22 -0700

    💫 Style: update posts-list ui with lazy loading

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -3,11 +3,12 @@ import { Observable } from 'rxjs';
 import { IPost } from '../../model/post.interface';
 import { ApiFacadeService } from '../../model/services/api-facade.service';
 import { CommonModule } from '@angular/common';
+import { RevealOnScrollDirective } from '../../directive/revealon-scroll.directive';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RevealOnScrollDirective],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss'
 })
---
+++ src/app/directive/revealon-scroll.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
+
+@Directive({
+  selector: '[appRevealOnScroll]',
+  standalone: true
+})
+
+export class RevealOnScrollDirective implements AfterViewInit {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.renderer.addClass(this.el.nativeElement, 'revealed');
+        } else {
+          this.renderer.removeClass(this.el.nativeElement, 'revealed');
+        }
+      });
+    }, { threshold: 0.9 ,  });
+
+    observer.observe(this.el.nativeElement);
+  }
+}
